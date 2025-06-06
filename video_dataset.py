69940c4f1c44d97a23c298a0199ff410bf7cb561
dhruvpendharkar <dhruv.pendharkar@gmail.com>
2025-06-05 23:18:00 -0500

    video dataset class

---
+++ video_dataset.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+import torch
+from torch.utils.data import Dataset
+
+class VideoDataset(Dataset):
+    def __init__(self, folder_path, num_videos=1000, transform=None):
+        self.folder_path = folder_path
+        self.transform = transform
+        video_files = os.listdir(folder_path)
+        self.video_files = video_files[:num_videos]
+
+    def __len__(self):
+        return len(self.video_files)
+
+    def __getitem__(self, idx):
+        video_path = os.path.join(self.folder_path, self.video_files[idx])
+        video = self.load_video(video_path)
+        
+        if self.transform:
+            video = self.transform(video)
+        
+        return video
+
+    def load_video(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+        frames = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frames.append(frame)
+        cap.release()
+        return torch.tensor(frames)
+
